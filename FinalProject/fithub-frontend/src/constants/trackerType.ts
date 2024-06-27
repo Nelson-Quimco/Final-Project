b4322a0b47d9eb5503b373054ae2d1d2f6fdd583
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-06-27 15:25:34 +0800

    add tracker type

---
+++ FinalProject/fithub-frontend/src/constants/trackerType.ts
@@ -0,0 +1,35 @@
+import User from "./userTypes";
+
+// Define enums for Level and Types
+enum Level {
+  BEGINNER = "BEGINNER",
+  INTERMEDIATE = "INTERMEDIATE",
+  ADVANCED = "ADVANCED",
+}
+
+enum Types {
+  CARDIO = "CARDIO",
+  STRENGTH = "STRENGTH",
+  FLEXIBILITY = "FLEXIBILITY",
+  BALANCE = "BALANCE",
+}
+
+// Define the FitnessExercise type
+interface FitnessExercise {
+  id: number;
+  Level: Level;
+  Type: Types;
+  Name: string;
+  Description: string;
+  createdAt: Date;
+  updatedAt: Date;
+  user: User;
+  userId: number;
+  //   progresses: Progress[];
+}
+
+interface FitnessExerciseResponse {
+  data: FitnessExercise[];
+}
+
+export type { FitnessExercise, FitnessExerciseResponse };
