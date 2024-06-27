0bd3047793359a02b03708c823f3fa5e53a5b0d4
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-06-27 16:36:56 +0800

    add initial UI for tracker and add workout page

---
+++ FinalProject/fithub-frontend/src/app/(pages)/tracker/add-workout/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import Button from "@/components/buttons/Button";
+import useTrackerRequest from "@/hooks/requests/tracker/useTrackerRequest";
+import React, { useEffect } from "react";
+import { IoEye } from "react-icons/io5";
+
+const AddWorkout = () => {
+  const { data, getExercises } = useTrackerRequest();
+
+  useEffect(() => {
+    getExercises();
+  }, []);
+
+  const viewExercises = (id: number) => {
+    console.log();
+  };
+
+  return (
+    <div className="w-full h-full bg-white shadow-md border-none rounded-md p-5 flex flex-col">
+      <div className="font-bold text-[20px] mb-4">ADD NEW WORKOUT</div>
+      <div className="w-full flex-grow flex gap-6">
+        <div className="w-[70%] h-full border rounded-md"></div>
+        <div className="w-[30%] h-full border rounded-md p-3 flex flex-col">
+          <p className="font-bold text-[20px] mb-4">Available Workout</p>
+          <div className="flex-grow h-96 overflow-y-auto">
+            {data && data.data ? (
+              data.data.map((x) => (
+                <div
+                  key={x.id}
+                  className="flex items-center gap-4 p-2 border-b"
+                >
+                  <Button name="add" width="60px" className=""></Button>
+                  <div>
+                    <p>{x.Name}</p>
+                    <p>Type: {x.Type}</p>
+                  </div>
+                  <IoEye onClick={() => viewExercises(x.id)} />
+                </div>
+              ))
+            ) : (
+              <div>No Data</div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddWorkout;
--- FinalProject/fithub-frontend/src/app/(pages)/tracker/page.tsx
+++ FinalProject/fithub-frontend/src/app/(pages)/tracker/page.tsx
@@ -1,9 +1,39 @@
 "use client";
-import withAuth from "@/components/auth/withAuth";
-import React from "react";
 
-const Tracker = () => {
-  return <div>Tracker</div>;
+import withAuth from "@/components/auth/withAuth";
+import Button from "@/components/buttons/Button";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const Tracker: React.FC = () => {
+  const router = useRouter();
+
+  return (
+    <div className="h-[85%]">
+      <Button
+        name="Add New Workout"
+        width="13rem"
+        height="3rem"
+        onClick={() => router.push("/tracker/addWorkout")}
+      ></Button>
+      <div className="flex w-full gap-10 h-[100%] mt-[2rem]">
+        <div className="flex flex-col gap-5 w-[70%]">
+          <div className=" h-[20%] mb-10">
+            Today's Workout:
+            <div className="border h-full"></div>
+          </div>
+          <div className=" h-[80%]">
+            Tomorrow's Workout:
+            <div className="border h-full"></div>
+          </div>
+        </div>
+        <div className=" w-[30%]">
+          Achievements:
+          <div className="border h-full"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default withAuth(Tracker);
