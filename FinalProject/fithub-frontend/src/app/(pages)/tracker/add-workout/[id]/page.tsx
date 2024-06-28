a083cd1de88690052da49f242482dff68ea318d2
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-06-28 16:53:55 +0800

    initial tracker module commit

---
+++ FinalProject/fithub-frontend/src/app/(pages)/tracker/add-workout/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = ({ params }: { params: { params: number } }) => {
+  return <div></div>;
+};
+
+export default page;
--- FinalProject/fithub-frontend/src/hooks/requests/tracker/useTrackerRequest.ts
+++ FinalProject/fithub-frontend/src/hooks/requests/tracker/useTrackerRequest.ts
@@ -6,6 +6,8 @@ const axiosReq = axios.create({ baseURL: process.env.NEXT_PUBLIC_URL });
 
 const useTrackerRequest = () => {
   const [data, setData] = useState<FitnessExerciseResponse | null>(null);
+  const [filteredData, setFilteredData] =
+    useState<FitnessExerciseResponse | null>(null);
 
   const getExercises = async () => {
     try {
@@ -17,11 +19,23 @@ const useTrackerRequest = () => {
     }
   };
 
+  const filterExercise = async (type: string, level: string) => {
+    try {
+      const res = await axiosReq.get(
+        `/fitness-tracking/type/${type}/level/${level}`
+      );
+      setFilteredData(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log("no data");
+    }
+  };
+
   useEffect(() => {
     getExercises();
   }, []);
 
-  return { data, getExercises };
+  return { data, getExercises, filteredData, filterExercise };
 };
 
 export default useTrackerRequest;
